9b05ea9727cf2193227eb3af531f14c87c84831b
Hector Padron <helpad93@gmail.com>
2023-08-24 21:40:41 +0000

    Finalized

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -21,10 +22,58 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
+			syncTokenFromSessionStore: () => {
+				const token = sessionStorage.getItem("token");
+				console.log("starting")
+				if(token && token != "" && token != undefined) setStore({ token: token });
+			},
+
+			logout: () => {
+				sessionStorage.removeItem("token");
+				console.log("login out")
+				setStore({ token: null });
+			},
+
+			Login: async (email, password) => {
+				const opts = {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				};
+				try{
+					const resp = await fetch('https://potential-couscous-rq6v4jwxjvvhpr75-3001.app.github.dev/api/token', opts)
+						if(resp.status !== 200){
+							alert("There has been some error");
+							return false;
+						}
+
+					const data = await resp.json();
+						console.log(data)
+						sessionStorage.setItem("token", data.access_token);
+						setStore({token: data.access_token});
+						return true;
+				
+				}
+				catch(error){
+					console.error("There has been an error login in")
+				}
+			},
+
 			getMessage: async () => {
+				const store = getStore();
+				const opts = {
+					headers:{
+						"Authorization": "Bearer" + store.token
+					}
+				}
 				try{
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const resp = await fetch("https://potential-couscous-rq6v4jwxjvvhpr75-3001.app.github.dev/api/hello", opts)
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
